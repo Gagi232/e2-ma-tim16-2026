9cc7aac898c91b643561f45d9612832aefe47032
Aleksandra <aleksandrarudez12@gmail.com>
2026-05-07 20:52:47 +0200

    Connecting with Firebase

--- app/src/main/java/com/example/slagalica/MainActivity.java
+++ app/src/main/java/com/example/slagalica/MainActivity.java
@@ -15,6 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Inicijalizacija baze
+        com.google.firebase.firestore.FirebaseFirestore db = com.google.firebase.firestore.FirebaseFirestore.getInstance();
+
+// Pokušaj čitanja iz kolekcije koju si napravila
+        db.collection("users").get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        android.util.Log.d("FIREBASE_PROVERA", "RADI! Pronađeno dokumenata: " + task.getResult().size());
+                    } else {
+                        android.util.Log.e("FIREBASE_PROVERA", "GREŠKA: ", task.getException());
+                    }
+                });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

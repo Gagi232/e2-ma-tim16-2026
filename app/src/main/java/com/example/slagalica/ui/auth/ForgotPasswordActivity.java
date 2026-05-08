1ce2233d630883041d682f20b9561b6766a09142
Aleksandra <aleksandrarudez12@gmail.com>
2026-05-08 02:12:17 +0200

    feat: GUI setup - Login, Register, Profile, KoZnaZna, Spojnice, Firebase, strukture

---
+++ app/src/main/java/com/example/slagalica/ui/auth/ForgotPasswordActivity.java
@@ -0,0 +1,13 @@
+package com.example.slagalica.ui.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.slagalica.R;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
---
+++ app/src/main/java/com/example/slagalica/ui/profile/ProfileActivity.java
@@ -0,0 +1,13 @@
+package com.example.slagalica.ui.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.slagalica.R;
+
+public class ProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
